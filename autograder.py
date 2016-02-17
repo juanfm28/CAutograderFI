845972a490b825a8f18c2ac58ddef546c1aab708
Juan Fonseca <fonseca.juan8@gmail.com>
2016-02-16 19:39:51 -0600

    Agregada compilacion de codigos fuente en .c
    En el autograder se agrego el parseo de las opciones del comando
    En el tester se creo la funcion para ejecutar todos los test
    En esta primero se usa una funcion para parsear parte del archivo
    Se agrego tambien la funcion que compila el codigo fuente

--- autograder.py
+++ autograder.py
@@ -1,5 +1,19 @@
 # -*- coding: utf-8 -*-
 
 #Programa principal que realiza el testing de un programa
+import argparse
+import tester
 
-pass
+parser = argparse.ArgumentParser(description='Selecciona las opciones de entrada')
+#Archivo de donde vienen las preguntas
+parser.add_argument('-t','--tests',action='store')
+#Parseo de las opciones
+args = parser.parse_args()
+
+if not args.tests:
+    print("Se requiere un archivo de pruebas")
+    exit()
+
+testFile = args.tests
+
+tester.runAllTests(testFile)
--- tester.py
+++ tester.py
@@ -1,5 +1,40 @@
 # -*- coding: utf-8 -*-
 
 #Modulo de asserts
+from os.path import isfile
+from subprocess import Popen,PIPE
 
-pass
+def runAllTest(testFile):
+    programNames = getProgramNames(testFile)
+    for program in programNames:
+    	compileSource(program+".c")
+    	pass
+
+def getProgramNames(testFile):
+    names = []
+    try:
+        test = open(testFile,'r')
+    except FileNotFoundError:
+        print("Archivo de pruebas no encontrado: ",testFile)
+
+    for line in test:
+        line = line.replace('\n','');
+        if line != "" and line.startswith('##'):
+            names.append(line.replace('##',''))
+    test.close()
+    return names
+
+def compileSource(sourcefile):
+    """Funcion que se encarga de ejecutar el compilador gcc sobre un archivo fuente"""
+    if not isfile(sourcefile):
+        print("Codigo fuente no encontrado: ",sourcefile)
+    else:
+        cmd = 'gcc -o '+sourcefile.replace('.c','')+' '+sourcefile
+        proc = Popen(cmd , shell=True, stdout=PIPE, stderr=PIPE)
+        out, err = proc.communicate()
+        if proc.returncode == 0:
+            print("Programa compilado con exito: ",sourcefile)
+        else:
+            print("Falló la compilación del programa: "+sourcefile+"\nErrores: ")
+            print(err.rstrip().decode('utf-8'))
+    return True
